48eb3674bc5d022b314184ea5ad310a4edd57a1b
Illes Andrei <andrei.illes03@e-uvt.ro>
2023-06-05 23:44:36 +0300

    fixat dropdown

--- script/dropdown.js
+++
@@ -1,6 +0,0 @@
-var dropdownLinks = dropdowns[i].getElementsByClassName("dropdown-content")[0].getElementsByTagName("a");
-      for (var j = 0; j < dropdownLinks.length; j++) {
-      dropdownLinks[j].addEventListener("click", function() {
-        window.location.href = this.href;
-      });
-    }
